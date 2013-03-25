a59f905a4b760884c815cb78f9c82791bd3a18fd
tdesvenain <thomas.desvenain@gmail.com>
2013-03-25 11:37:18 +0100

    i18n 'Collaborate'

--- collective/etherpad/rebuild_i18n.sh
+++ collective/etherpad/rebuild_i18n.sh
@@ -4,6 +4,8 @@ I18NDOMAIN=$PRODUCTNAME
 
 # Synchronise the .pot with the templates.
 i18ndude rebuild-pot --pot locales/${PRODUCTNAME}.pot --create ${I18NDOMAIN} .
+i18ndude rebuild-pot --pot locales/plone.pot --create plone .
 
 # Synchronise the resulting .pot with the .po files
 i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/*/LC_MESSAGES/${PRODUCTNAME}.po
+i18ndude sync --pot locales/plone.pot locales/*/LC_MESSAGES/plone.po
